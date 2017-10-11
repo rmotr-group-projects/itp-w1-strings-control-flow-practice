0b20206432e8f15503afb83b6c0be0523cdb6a90
Amy McCarther <amy.mccarther@yahoo.com>
2017-10-11 02:52:58 +0000

    Assignment 4

--- group_project/assignment_4.py
+++ group_project/assignment_4.py
@@ -1,5 +1,20 @@
 def how_many_times(a_string, a_word):
-    pass
+    if a_word in a_string:
+        return a_string.count(a_word)
+    if not a_word in a_string:
+        return 0
+   
+    
+
+phrase_1 = "Python is a great language. I like Python a lot."
+phrase_2 = "Python is a great language. I like it a lot."
+phrase_3 = "Python is a great language."
+
+print(how_many_times(phrase_1,"Python"))
+print(how_many_times(phrase_2,"Python"))
+print(how_many_times(phrase_3,"Ruby"))
+
+
 
 
 def test_more_than_once():
