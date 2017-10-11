c0bec00faf701ef1e67642ea3491642634ffb809
Amy McCarther <amy.mccarther@yahoo.com>
2017-10-11 02:54:31 +0000

    Assignment 5

--- group_project/assignment_5.py
+++ group_project/assignment_5.py
@@ -1,5 +1,31 @@
 def positions(a_string, first_word, second_word, third_word):
-    pass
+    pos_1st = a_string.find(first_word)
+    pos_2nd = a_string.find(second_word)
+    pos_3rd = a_string.find(third_word)
+    if pos_1st == -1:
+        pos_1st = '-'
+    else:
+        pos_1st = str(pos_1st)
+    if pos_2nd == -1:
+        pos_2nd = '-'
+    else:
+        pos_2nd = str(pos_2nd)
+    if pos_3rd == -1:
+        pos_3rd = '-'
+    else:
+        pos_3rd = str(pos_3rd)
+    
+    myfinalresult = pos_1st + ',' + pos_2nd + ',' + pos_3rd
+    return myfinalresult
+
+    
+
+print(positions("Python is good. Python is Wise. I like Python", 'Python', 'Wise', 'like'))
+print(positions("Python is good. Python is Wise. I like Python",'Python', 'Wise', 'Ruby'))
+print(positions("Python is good. Python is Wise. I like Python", 'Ruby', 'Wise', 'like'))
+print(positions("Python is good. Python is Wise. I like Python", 'Python', 'Javascript', 'Ruby'))
+print(positions("Python is good. Python is Wise. I like Python", 'Javscript', 'like', 'Ruby'))
+print(positions("Python is good. Python is Wise. I like Python", 'Javscript', 'Ruby', 'Wise'))
 
 
 def test_three_occurrences():
