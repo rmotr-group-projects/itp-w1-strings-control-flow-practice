f25e578eb5f4774fbc83a3387ad5d34bf35421bb
Michael S <michael_j_smythe@yahoo.com>
2017-10-11 02:39:12 +0000

    Assignment 1 Working

--- group_project/assignment_1.py
+++ group_project/assignment_1.py
@@ -1,6 +1,7 @@
 def first_half(a_string):
-    pass
-
+    halflength = int(round(len(a_string)/2.0))
+    return a_string[0:halflength]
+        
 
 def test_first_half_with_even_length_string():
     assert first_half('abcdef') == 'abc'
