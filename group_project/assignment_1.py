bc3f4430905d06c080919bc934fe6fedd5e47a0c
Adriana <aspatterson@gmail.com>
2017-10-11 16:25:33 +0000

    Adriana's solution to assign. 1

--- group_project/assignment_1.py
+++ group_project/assignment_1.py
@@ -1,5 +1,8 @@
 def first_half(a_string):
-    pass
+    if len(a_string) % 2 == 0:
+        return a_string[0:len(a_string)/2]
+    else:
+        return a_string[0:len(a_string)/2] + 'X'
 
 
 def test_first_half_with_even_length_string():
