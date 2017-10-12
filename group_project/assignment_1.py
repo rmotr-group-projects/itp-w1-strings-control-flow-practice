95263de174628dac7c02b75857abe5f7efa3d02c
nsevryna <nataliya.sevryna@gmail.com>
2017-10-11 22:52:01 -0400

    Update assignment_1.py

--- group_project/assignment_1.py
+++ group_project/assignment_1.py
@@ -1,9 +1,9 @@
 def first_half(a_string):
     if len(a_string) >=1:
         if len(a_string) % 2 == 0:
-            return a_string[0:len(a_string)/2]
+            return a_string[0:int(len(a_string)/2)]
         elif len(a_string) % 2 != 0:
-            return a_string[0:((len(a_string)+1)/2)]
+            return a_string[0:int((len(a_string)+1)/2)]
     return 'No string Found'
 
 def test_first_half_with_even_length_string():
