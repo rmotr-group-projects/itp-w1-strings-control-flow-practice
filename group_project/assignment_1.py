98a0a1ecae6c4a2f0c328160d69541f82efc934c
Julia <jukostyuchenko@gmail.com>
2017-10-12 17:54:49 +0000

    assignment_1 working

--- group_project/assignment_1.py
+++ group_project/assignment_1.py
@@ -1,5 +1,11 @@
 def first_half(a_string):
-    pass
+    length = len(a_string)
+    half = a_string[0:length / 2]
+    
+    if length % 2 == 0:
+        return half
+    else:
+        return a_string[0:length / 2 + 1]
 
 
 def test_first_half_with_even_length_string():
