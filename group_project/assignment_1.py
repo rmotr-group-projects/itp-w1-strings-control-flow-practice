a270dde6b805898754c0dd38f5c674f7189a61fc
Viswanath <lordvissu@gmail.com>
2017-10-13 15:56:40 +0000

    All Assignments working NOW

--- group_project/assignment_1.py
+++ group_project/assignment_1.py
@@ -4,8 +4,6 @@ def first_half(a_string):
     else:
         return a_string[0:((len(a_string)/2)+1)]
 
-print first_half("abcXdef")
-
 def test_first_half_with_even_length_string():
     assert first_half('abcdef') == 'abc'
 
