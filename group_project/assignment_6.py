35951a3d2b5e4a4292bfc74759055cf6ca97fbe1
Eric Drake <drakes2005@gmail.com>
2017-10-16 19:48:04 +0000

    "assigment_6_correction"

--- group_project/assignment_6.py
+++ group_project/assignment_6.py
@@ -19,7 +19,7 @@ def format_tic_tac_toe_board(first_row, second_row, third_row):
 
     return(board.format(first_row[0],first_row[1],first_row[2],second_row[0],second_row[1],second_row[2],third_row[0],third_row[1],third_row[2]))
 
-print format_tic_tac_toe_board('XOX','OXO','OOX')
+
 
 def test_format_board():
     """
